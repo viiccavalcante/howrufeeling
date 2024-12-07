4d1b9365aaa1f7a3ffbf2860b231bc2e01156e63
victoria <victoriapcavalcante@gmail.com>
2024-12-07 16:35:34 +0100

    creating functionality to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import ListSelected from "./pages/ListSelected";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const feelings =["Joy", "Excitement", "Love", "Gratitude", "Contentment",
+    "Pride", "Hope", "Curiosity", "Peace", "Inspiration",
+    "Confidence", "Enthusiasm", "Relief", "Amusement",
+    "Awe", "Optimism", "Compassion", "Satisfaction",
+    "Trust", "Serenity", "Anger", "Sadness", "Fear",
+    "Guilt", "Shame", "Disappointment", "Frustration",
+    "Anxiety", "Loneliness", "Jealousy", "Envy",
+    "Resentment", "Embarrassment", "Despair", "Grief",
+    "Helplessness", "Regret", "Vulnerability", "Overwhelm", "Bitterness",
+    "Nostalgia", "Confusion", "Anticipation", "Surprise", "Indifference", "Yearning",
+    "Suspense", "Longing", "Restlessness","Melancholy"];
+
+  //const [showListSelected, setShowHListSelected] = useState(false);
+  const [selectedFeelings, setSelectedFeelings] = useState([]);
+  const [activeFeelings, setActiveFeelings] = useState(feelings);
+  const [currentFeeling, setCurrentFeeling] = useState(activeFeelings[0]);
+
+    const generateFeeling = () => {
+      const position = Math.floor(Math.random() * activeFeelings.length);
+      setCurrentFeeling(activeFeelings[position]);
+    };
+
+    const removeFeeling = () => {
+      setActiveFeelings(activeFeelings.filter(item => item !== currentFeeling));
+    };
+
+    const selectFeeling = () => {
+      setSelectedFeelings([...selectedFeelings, currentFeeling]); 
+      removeFeeling();
+      generateFeeling();
+    };
+
+    const discardFeeling = () => {
+      removeFeeling();
+      generateFeeling();
+    };
+
+    const reconsiderFeeling = () => {
+      generateFeeling();
+    };
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <>
+          <h1>How are you feeling?</h1>
+          <div>
+            <h3>{currentFeeling}</h3>
+          </div>
+          <button onClick={selectFeeling}>Yes</button>
+          <button onClick={reconsiderFeeling}>Maybe</button>
+          <button onClick={discardFeeling}>No</button>
+        </>
+ 
+ 
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
